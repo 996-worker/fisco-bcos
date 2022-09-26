2d58e709619d7bcce93a1082f679d2345f26df09
996-worker <448140808@qq.com>
2022-09-27 02:30:10 +0800

    通知消息入库

--- src/main/java/com/post/fisco/contract/utils/BcosSdkClient.java
+++ src/main/java/com/post/fisco/contract/utils/BcosSdkClient.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.fisco.bcos.sdk.BcosSDK;
 import org.fisco.bcos.sdk.client.Client;
 import org.fisco.bcos.sdk.crypto.keypair.CryptoKeyPair;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,12 +20,17 @@ public class BcosSdkClient {
 
     public static Client getClient() {
         if (client == null) {
-            ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-            BcosSDK bcosSDK = context.getBean(BcosSDK.class);
-            client = bcosSDK.getClient(1);
-            cryptoKeyPair = client.getCryptoSuite().createKeyPair();
+            try {
+                ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+                BcosSDK bcosSDK = context.getBean(BcosSDK.class);
+                client = bcosSDK.getClient(1);
+                cryptoKeyPair = client.getCryptoSuite().createKeyPair();
 //            client.getCryptoSuite().setCryptoKeyPair(cryptoKeyPair);
-            log.info("=》连接Bcos客户端:" + cryptoKeyPair.getAddress());
+                log.info("=》连接Bcos客户端:" + cryptoKeyPair.getAddress());
+            } catch (BeansException e) {
+                log.error("=》连接Bcos客户端异常",e);
+                e.printStackTrace();
+            }
         }
         return client;
     }
